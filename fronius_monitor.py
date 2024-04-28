119879a3dd68bb86b64f4250029aebfadd0980be
Ben Johns <bjohns@naturalnetworks.net>
2024-04-28 19:34:01 +1000

    Fixed errors - none value checks etc.

--- fronius_monitor.py
+++ fronius_monitor.py
@@ -12,6 +12,7 @@ Author: Ben Johns (bjohns@naturalnetworks.net)
 
 import time
 import json
+import traceback
 import requests
 import paho.mqtt.client as mqtt
 
@@ -36,27 +37,35 @@ def main(interval_seconds):
     while True:
         try:
             # Retrieve data from Fronius API
-            froniusData = get_froniusData(froniusIp, "/solar_api/v1/GetPowerFlowRealtimeData.fcgi") 
-            pvLoad = froniusData['Body']['Data']['Site']['P_Load']
-            pvGeneration = froniusData['Body']['Data']['Site']['P_PV']
-            pvGrid = froniusData['Body']['Data']['Site']['P_Grid']
+            froniusData = get_froniusData(froniusIp, "/solar_api/v1/GetPowerFlowRealtimeData.fcgi")
+            pvLoad = froniusData['Body']['Data']['Site'].get('P_Load', 0)
+            pvGeneration = froniusData['Body']['Data']['Site'].get('P_PV', 0)
+            pvGrid = froniusData['Body']['Data']['Site'].get('P_Grid', 0)
 
             froniusData_common = get_froniusData(froniusIp, "/solar_api/v1/GetInverterRealtimeData.cgi?Scope=Device&DeviceId=1&DataCollection=CommonInverterData")
-            pvGridVoltage = froniusData_common['Body']['Data']['UAC']['Value']
-            pvGridFrequency = froniusData_common['Body']['Data']['FAC']['Value']
+            pvGridVoltage = froniusData_common['Body']['Data'].get('UAC', {}).get('Value', 0)
+            pvGridFrequency = froniusData_common['Body']['Data'].get('FAC', {}).get('Value', 0)
 
             froniusData_meter = get_froniusData(froniusIp, "/solar_api/v1/GetMeterRealtimeData.cgi?Scope=Device&DeviceId=0")
-            pvMeterGridVoltage = froniusData_meter['Body']['Data']['Voltage_AC_Phase_1']
-            pvMeterGridFrequency = froniusData_meter['Body']['Data']['Frequency_Phase_Average']
-            pvMeterGridPf = froniusData_meter['Body']['Data']['PowerFactor_Phase_1']
+            pvMeterGridVoltage = froniusData_meter['Body']['Data'].get('Voltage_AC_Phase_1', 0)
+            pvMeterGridFrequency = froniusData_meter['Body']['Data'].get('Frequency_Phase_Average', 0)
+            pvMeterGridPf = froniusData_meter['Body']['Data'].get('PowerFactor_Phase_1', 0)
+
+            # Check if both pvGeneration and pvLoad are valid
+            if pvGeneration is not None and pvLoad is not None:
+                 pvImport = pvGeneration - pvLoad
+                 pvExport = pvLoad - pvGeneration
+            else:
+                 pvImport = 0
+                 pvExport = 0
 
             # Generate MQTT payload
             device_id = 1  # Assuming device_id is 1
             mqtt_payload = {
                 "sensorID": device_id,
                 "timecollected": int(time.time()),
-                "pvImport": pvGeneration - pvLoad,
-                "pvExport": pvLoad - pvGeneration,
+                "pvImport": pvImport,
+                "pvExport": pvExport,
                 "pvGeneration": pvGeneration,
                 "pvLoad": pvLoad,
                 "gridVoltage": pvGridVoltage,
@@ -75,9 +84,11 @@ def main(interval_seconds):
             time.sleep(interval_seconds)
 
         except Exception as e:
-            print(f"An error occurred: {str(e)}")
+            # print(f"An error occurred: {str(e)}")
+            print("An error occurred:")
+            print(str(e))
+            traceback.print_exc()
 
 if __name__ == "__main__":
     interval_seconds = 5
     main(interval_seconds)
-
